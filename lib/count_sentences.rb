5f5fa3a302f6c3cdac462877af09ccf807d200af
Mai Vang <vmaineng@gmail.com>
2022-06-07 17:11:57 -0700

    commit tree 40a35d2c6d9444fe282d4e5784f4262dc6f221f1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
+  
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
+
+"The weather outside is frightful.".sentence?
+"The weather outside is frightful.".question?
+"The weather outside is frightful.".exclamation?
+"This is a string! It has three sentences. Right?".count_sentences
